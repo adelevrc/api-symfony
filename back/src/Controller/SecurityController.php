a03c196f719d17858f8b33ee0291acfe601c9638
Jocelyn Griselle <dev@griselle.io>
2021-03-07 20:47:29 +0100

    don’t use crappy lexik bundle

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -3,77 +3,65 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Firebase\JWT\JWT;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
 
 class SecurityController extends AbstractController
 {
-
     /**
-     * @Route(name="api_login", path="/api/login_check")
-     * @return JsonResponse
+     * @Route("/auth/login", name="login", methods={"POST"})
      */
-    public function api_login(LoggerInterface $logger): JsonResponse
+    public function login(Request $request, UserRepository $userRepository, UserPasswordEncoderInterface $encoder)
     {
-        $user = $this->getUser();
-        $logger->error($user);
-        if ($user == null) {
-            return new JsonResponse([
-                "NO NO NO"
+
+        $data = json_decode($request->getContent(), true);
+        $email = $data["email"];
+        $password = $data["password"];
+        $user = $userRepository->findOneBy([
+            'email'=>$email,
+        ]);
+        if (!$user || !$encoder->isPasswordValid($user, $password)) {
+            return $this->json([
+                'message' => 'email or password is wrong.',
             ]);
         }
+        $payload = [
+            "user" => $user->getUsername(),
+            "exp"  => (new \DateTime())->modify("+5 minutes")->getTimestamp(),
+        ];
 
-        return new JsonResponse([
-            'email' => $user->getEmail(),
-            'roles' => $user->getRoles(),
+        $jwt = JWT::encode($payload, $this->getParameter('jwt_secret'), 'HS256');
+        return $this->json([
+            'message' => 'success!',
+            'token' => sprintf($jwt),
         ]);
     }
 
     /**
-     * @Route("/api/login", name="app_login")
+     * @Route("/auth/register", name="register", methods={"POST"})
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
-
-    /**
-     * @Route("/logout", name="app_logout", methods={"GET"})
-     */
-    public function logout()
-    {
-        // controller can be blank: it will never be executed!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
-    }
-
-    /**
-     * @Route("/api/register", name="register", methods={"POST"})
-     */
-    public function register(Request $request, UserPasswordEncoderInterface $encoder, LoggerInterface $logger)
-    {
-        $em = $this->getDoctrine()->getManager();
         $data = json_decode($request->getContent(), true);
         $email = $data["email"];
         $password = $data["password"];
-        $logger->info($email);
         $user = new User();
-        $user->setEmail($email);
-
         $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setEmail($email);
+        $em = $this->getDoctrine()->getManager();
         $em->persist($user);
         $em->flush();
-
-        return new JsonResponse(sprintf('User %s successfully created', $user->getUsername()));
+        return $this->json([
+            'user' => $user->getEmail()
+        ]);
     }
 }
